2dc8b92346df3091b24632f06b95f25079b94bd7
ravisuneel6565 <ravisuneel6262@gmail.com>
2024-05-10 15:44:46 +0530

    .

---
+++ Frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div>
+      <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourrite food here</h2>
+            <p>choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise . Our mission is to satisfy your cravings and elevate your diing experience , one delicious mea at a time.</p>
+            <button>View Menu</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+import './Header.css'
+export default Header
---
+++ Frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+import Header from '../../components/Header/Header'
+
+const home = () => {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+export default home
