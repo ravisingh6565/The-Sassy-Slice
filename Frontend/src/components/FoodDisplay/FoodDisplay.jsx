6cee469af01083bfea9966f1045651b20d6e57ba
ravisuneel6565 <ravisuneel6262@gmail.com>
2024-05-11 23:13:30 +0530

    .

---
+++ Frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+import "./FoodDisplay.css";
+//it is not works because Store context is already inn use
+// import { StoreContext } from '../../context/StoreContext'
+
+const FoodDisplay = ({ category }) => {
+  const { food_list } = useContext(StoreContext);
+
+  return (
+    <div className="food-display" id="food-display">
+      <h2>Top dishes near you</h2>
+      <div className="food-display-list">
+        {food_list.map((item, index) => {
+          return (
+            <FoodItem
+              key={index}
+              id={item.id}
+              name={item.name}
+              description={item.description}
+              price={item.price}
+              image={item.image}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+import "./FoodDisplay.css";
+import StoreContextProvider, { StoreContext } from "../../context/StoreContext";
+import FoodItem from "../FoodItem/FoodItem";
+export default FoodDisplay;
--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
       <div className='header'>
         <div className="header-contents">
             <h2>Order your favourrite food here</h2>
-            <p>choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise . Our mission is to satisfy your cravings and elevate your diing experience , one delicious mea at a time.</p>
+            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise . Our mission is to satisfy your cravings and elevate your diing experience , one delicious mea at a time.</p>
             <button>View Menu</button>
         </div>
       </div>
